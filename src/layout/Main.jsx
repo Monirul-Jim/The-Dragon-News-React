edb0b1e77eea282eab27bd80bdf024c0cd56d5fa
unknown <Monirul-Jim>
2023-04-18 22:36:11 +0600

    right side half

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,7 @@ import Header from '../pages/shared/Header/Header';
 import Footer from '../pages/shared/footer/Footer';
 import { Button, Col, Container, ListGroup, Row } from 'react-bootstrap';
 import RightSideNav from '../pages/rightsidenav/RightSideNav';
+import LeftSideNav from '../pages/leftsidenav/LeftSideNav';
 
 const Main = () => {
     return (
@@ -11,9 +12,13 @@ const Main = () => {
 
             <Container>
                 <Row>
-                    <Col lg={3}>this is col 3</Col>
+                    <Col lg={3}>
+                        <LeftSideNav></LeftSideNav>
+                    </Col>
                     <Col lg={6}>this is col 6</Col>
-                    <Col lg={3}><RightSideNav></RightSideNav> </Col>
+                    <Col lg={3}>
+                        <RightSideNav></RightSideNav>
+                    </Col>
                 </Row>
             </Container>
             <Footer></Footer>
